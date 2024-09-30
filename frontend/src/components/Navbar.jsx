003b1377a83ff55dcd51b588cf733f9e0a720d61
tempestpulse <bulsar@tutanota.com>
2024-09-30 20:01:07 +0200

    add MoodEntryForm to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,40 +1,44 @@
 import React, { useState, useEffect } from "react";
 import api from "../api";
+import MoodEntryForm from "./MoodEntryForm";
 
 function Navbar({ userId }) {
-    const [username, setUsername] = useState("")
-    useEffect(() => {
-        console.log('userId:', userId);
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    console.log("userId:", userId);
 
-        if (userId) {
-            api.get(`/api/users/${userId}/`)
-                .then(res => {
-                    console.log('API Response:', res.data);
-                    setUsername(res.data.username);
-                })
-                .catch(err => {
-                    console.error('API Error:', err);
-                });
-        } else {
-            console.warn('No userId available');
-        }
-    }, [userId]);
+    if (userId) {
+      api
+        .get(`/api/users/${userId}/`)
+        .then((res) => {
+          console.log("API Response:", res.data);
+          setUsername(res.data.username);
+        })
+        .catch((err) => {
+          console.error("API Error:", err);
+        });
+    } else {
+      console.warn("No userId available");
+    }
+  }, [userId]);
 
-    return (
-        <div className="navbar bg-base-100">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Mood Tracker</a>
-            </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Add Mood</a></li>
-                    <li>
-                        <a>{username}</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Mood Tracker</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <MoodEntryForm userId={userId} />
+          </li>
+          <li>
+            <a>{username}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
